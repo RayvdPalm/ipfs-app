17bce33142ebed353c16d0f6230286908b157f40
Raynold van der Palm <rayvanderpalm@outlook.com>
2022-03-14 15:00:30 +0100

    moralis ipfs
    AAAAAAAAAAAAAAAAAAAAH

---
+++ Moralis project ipfs/login/main.js
@@ -0,0 +1,27 @@
+// main.js
+
+const serverUrl = "https://rkrmvjgt02d8.usemoralis.com:2053/server";
+const appId = "5DKM4WVSui4kf4BaTRzeqRroHnwROLb5VJXtVAda";
+Moralis.start({ serverUrl, appId });
+
+/** Add from here down */
+async function login() {
+  let user = Moralis.User.current();
+  if (!user) {
+   try {
+      user = await Moralis.authenticate({ signingMessage: "Hello World!" })
+      console.log(user)
+      console.log(user.get('ethAddress'))
+   } catch(error) {
+     console.log(error)
+   }
+  }
+}
+
+async function logOut() {
+  await Moralis.User.logOut();
+  console.log("logged out");
+}
+
+document.getElementById("btn-login").onclick = login;
+document.getElementById("btn-logout").onclick = logOut;
